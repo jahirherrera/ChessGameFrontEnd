f3dd0098cb3d3338dff2069c92c1d3e6d2305657
Jahir Herrera <jahirherrera12345@gmail.com>
2026-01-13 11:53:13 -0500

    this is it

---
+++ ChessProject/src/homePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage(){
+    return(
+        <div>
+            <h1>Welcome to the Chess Game!</h1>
+            <button>Play Game Online</button>
+        </div>
+    )
+}
